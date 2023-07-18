8c186388f441bed3d11b863119b6e9a732f31f7b
Sean Norwood <norwood.sean@gmail.com>
2023-07-17 19:54:13 -0500

    Add logic

--- src/triangle.js
+++ src/triangle.js
@@ -5,5 +5,19 @@ export default function Triangle(side1, side2, side3) {
 }
 
 Triangle.prototype.checkType = function () {
-  return "I can't answer that yet!";
+  if (this.side1 <= 0 || this.side2 <= 0 || this.side3 <= 0) {
+    return "not a triangle";
+  } else if (
+    this.side1 + this.side2 <= this.side3 ||
+    this.side1 + this.side3 <= this.side2 ||
+    this.side2 + this.side3 <= this.side1
+  ) {
+    return "not a triangle";
+  } else if (this.side1 === this.side2 && this.side1 === this.side3) {
+    return "equilateral triangle";
+  } else if (this.side1 === this.side2 || this.side1 === this.side3 || this.side2 === this.side3) {
+    return "isosceles triangle";
+  } else {
+    return "scalene triangle";
+  }
 };
